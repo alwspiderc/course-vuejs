1b602f14895d83efe8c49143dae3f4232de2551a
Ester Costa de Souza <alwesterc@gmail.com>
2025-11-12 13:15:22 -0300

    projetos para aprtender a base do vuejs

---
+++ Project 5 - Shopping Bag Initial/src/store/index.js
@@ -0,0 +1,56 @@
+import { createStore } from 'vuex';
+import axios from 'axios';
+
+export default createStore({
+	state: {
+		products: [],
+		productsInBag: []
+	},
+	mutations: {
+		loadProducts(state, products) {
+			state.products = products;
+		},
+		loadBag(state, products) {
+			state.productsInBag = products;
+		},
+		addToBag(state, product) {
+			state.productsInBag.push(product);
+			localStorage.setItem(
+				'productsInBag',
+				JSON.stringify(state.productsInBag)
+			);
+		},
+		removeFromBag(state, productId) {
+			var updatedBag = state.productsInBag.filter(
+				(product) => product.id !== productId
+			);
+			state.productsInBag = updatedBag;
+
+			localStorage.setItem(
+				'productsInBag',
+				JSON.stringify(state.productsInBag)
+			);
+		}
+	},
+	actions: {
+		loadProducts({ commit }) {
+			axios.get('https://fakestoreapi.com/products').then((response) => {
+				commit('loadProducts', response.data);
+			});
+		},
+		loadBag({ commit }) {
+			if (localStorage.getItem('productsInBag')) {
+				commit('loadBag', JSON.parse(localStorage.getItem('productsInBag')));
+			}
+		},
+		addToBag({ commit }, product) {
+			commit('addToBag', product);
+		},
+		removeFromBag({ commit }, productId) {
+			if (confirm('Are you sure you want to remove this item from the bag?')) {
+				commit('removeFromBag', productId);
+			}
+		}
+	},
+	modules: {}
+});
---
+++ book-list/src/components/BookProgress.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { computed } from 'vue';
+const props = defineProps(['books']);
+
+const booksRead = computed(() => {
+	return props.books.filter((book) => book.isRead).length;
+});
+
+const booksReadMessage = computed(() => {
+	return booksRead.value >= props.books.length
+		? 'Parabéns! Você leu todos os livros!'
+		: `${booksRead.value} de ${props.books.length} livros lidos`;
+});
+</script>
+
+<template>
+	<div class="books-read">
+		<label for="progress"> Seu progresso</label>
+		<br />
+		<progress id="progress" :value="booksRead" :max="books.length"></progress>
+		<p>{{ booksReadMessage }}</p>
+	</div>
+</template>
